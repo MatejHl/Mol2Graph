89206d3405123de5c56b300b9e72fa1d3eb671f8
Matej Hladiš <matej.hladis@gmail.com>
2021-01-04 15:40:37 +0100

    sparse represantation changed in accordance with spektral in _to_spektral functions

--- mol2graph/convert.py
+++ mol2graph/convert.py
@@ -110,7 +110,7 @@ CHIRAL_TAG = {key : int(val) for key, val in Chem.rdchem.ChiralType.names.items(
 HYBRIDIZATION = {key : int(val) for key, val in Chem.rdchem.HybridizationType.names.items()}
 BOND_TYPE = {key : int(val) for key, val in Chem.rdchem.BondType.names.items()}
 
-def mol_to_spektral(mol, scipy_E = False):
+def mol_to_spektral(mol, y = None):
     """
     For now, this is just an example function. Copy it and change if you need 
     any other Atom/Bond attributes.
@@ -135,9 +135,9 @@ def mol_to_spektral(mol, scipy_E = False):
              is_aromatic]
 
         E = [bond_type]
-        if scipy_E is True then E is scipy.sparse.coo.coo_matrix of shape
-        (n_nodes, n_nodes, n_edge_features) which is symmetric in coordinates [0, 1]
-        else it is a numpy.ndarray
+        E is stored as a numpy array where elements are ordered by (row_idx, col_idx) (so similar 
+        to scipy.sparse.crs_matrix) and adjecancy matrix is used to extraxt which to which edge the
+        data corresponds. 
 
         Categorical features in X and E are encoded by rdkit.Chem.rdchem standard numbering and
         mapping from numbers to names can be accessed from CHIRAL_TAG, HYBRIDIZATION and BOND_TYPE
@@ -160,16 +160,20 @@ def mol_to_spektral(mol, scipy_E = False):
         end.append(bond.GetEndAtomIdx())
         E.append([int(bond.GetBondType())])
 
-    if scipy_E:
-        data = list(itertools.chain.from_iterable(E))
-        E = scipy.sparse.coo_matrix((data + data, (begin + end, end + begin)), shape = Chem.GetAdjacencyMatrix(mol).shape)
-    else:
-        E = np.array(E)
+    _E = list(zip(begin + end, end + begin, E + E))
+    _E.sort(key=lambda ele: (ele[0], ele[1]))
+    E = np.array([dat[2] for dat in _E], dtype=np.float32)
 
-    G = sp_Graph(x = np.array(X),
-                a = A,
+    assert len(E.shape) == 2
+
+    # data = list(itertools.chain.from_iterable(E))
+    # E = scipy.sparse.csr_matrix((data + data, (begin + end, end + begin)), shape = Chem.GetAdjacencyMatrix(mol).shape)
+    # E.sort_indices()
+
+    G = sp_Graph(x = np.array(X, dtype = np.float32),
+                a = A.astype(np.float32),
                 e = E,
-                y = None)
+                y = y)
     return G
 
 
@@ -177,7 +181,7 @@ def spektral_to_mol():
     pass
 
 
-def smiles_to_spektral(smiles, validate=False, scipy_E = False):
+def smiles_to_spektral(smiles, y = None, validate=False, scipy_E = False):
     """
     Convert SMILES string to nx.Graph.
 
@@ -194,8 +198,32 @@ def smiles_to_spektral(smiles, validate=False, scipy_E = False):
     Internally this function creates RDkit molecule and uses mol_to_spektral(mol, scipy_E = False).
     """
     mol = Chem.MolFromSmiles(smiles.strip())
+    assert mol is not None
+
     can_smi = Chem.MolToSmiles(mol) # canonical SMILES - TO DO: Check if this row is necessary.
-    G = mol_to_spektral(mol, scipy_E = scipy_E)
+    G = mol_to_spektral(mol, y = y)
     if validate:
         raise NotImplementedError("validate = True is not implemented at the moment. Use validate = False.")
     return G
+
+
+def pdb_to_nx():
+    import os
+    import io
+    
+    # biopython
+    from Bio.PDB import PDBParser, PPBuilder, Chain
+    
+    
+    raise NotImplementedError('Needs to be finished and moved to smiles2graph')
+    
+    
+    def get_structure_by_path(pdb_id, pdb_path):
+        parser = PDBParser(QUIET=False)
+        return parser.get_structure(pdb_id, pdb_path)
+    
+    if __name__ == '__main__':
+        pdb_name = "Olfr263.B99991199.pdb"
+        pdb_path = os.path.join("pdb_test", pdb_name)
+    
+        fn = get_structure_by_path("Olfr263", pdb_path)
