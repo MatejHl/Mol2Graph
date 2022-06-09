15636fed61cfffb238d5da9a1de71f519732286c
Matej <matej.hladis@gmail.com>
2022-06-09 08:13:12 +0200

    fixing setup and adding __init__

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name='mol2graph', version='0.1', packages=find_packages())
+setup(name='mol2graph', 
+        version='0.1', 
+        packages=find_packages(
+            include=['mol2graph*']
+        ))
